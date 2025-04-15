d577da7beea1aa7f1a785f82d4b82c4a5070ccc1
Sammy <sammybeard2002@gmail.com>
2025-04-15 02:19:08 -0400

    Began implementing weather api

---
+++ src/weather_api.py
@@ -0,0 +1,50 @@
+import openmeteo_requests
+
+import requests_cache
+import pandas as pd
+from retry_requests import retry
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+
+# define endpoint and params (2m for measurements at 2 meters above ground level)
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+	"latitude": 33.9609,
+	"longitude": -83.3779,
+	"hourly": ["temperature_2m", "relative_humidity_2m", "precipitation_probability"]
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# process response (Athens, GA)
+response = responses[0]
+print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation {response.Elevation()} m asl")
+print(f"Timezone {response.Timezone()}{response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+# process hourly data, order of params is important 
+hourly = response.Hourly()
+hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
+hourly_precipitation_probability = hourly.Variables(2).ValuesAsNumpy()
+
+# initialize dictionary of data ranges to populate with data
+hourly_data = {"date": pd.date_range(
+	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = hourly.Interval()),
+	inclusive = "left"
+)}
+
+# add data to dictionary
+hourly_data["temperature_2m"] = hourly_temperature_2m
+hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
+hourly_data["precipitation_probability"] = hourly_precipitation_probability
+
+# create dataframe from dictionary
+hourly_dataframe = pd.DataFrame(data = hourly_data)
+print(hourly_dataframe)
